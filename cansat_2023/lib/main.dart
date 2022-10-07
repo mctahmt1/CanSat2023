3c6fb6b25025eba9a62a49a415c73f97260d8e82
Ahmet Macit <macitahmet65@gmail.com>
2022-10-07 03:50:32 +0300

    changes

--- cansat_2023/lib/main.dart
+++ cansat_2023/lib/main.dart
@@ -8,7 +8,12 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
